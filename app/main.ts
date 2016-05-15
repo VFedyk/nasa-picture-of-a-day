dab912111d3a9c026593476d3ac3709c3542a29c
Volodymyr <volodymyr.fedyk@gmail.com>
2016-05-15 14:23:11 +0000

    Added semicolons and configuration file for lite server

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
 import {HTTP_PROVIDERS} from "angular2/http";
 import {ApiService} from "./services/api.service";
 import {CacheService} from "./services/cache.service";
