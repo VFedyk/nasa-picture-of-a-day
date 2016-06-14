5f9f807c3e4f45679e2fdc2a00126a25b609ce3a
Volodymyr Fedyk <VFedyk@users.noreply.github.com>
2016-06-14 18:05:18 +0300

    Update api-key-form.component.ts

--- app/components/api-key-form.component.ts
+++ app/components/api-key-form.component.ts
@@ -4,7 +4,7 @@ import {DataService} from "../services/data.service";
 @Component({
     selector: 'api-key-form',
     template: `
-        <p>First of all you need to obtain NASA API-key. Follow <a href="https://api.nasa.gov/index.html#apply-for-an-api-key">this link</a> and get it.</p>
+        <p>First of all you need to obtain NASA API-key. Follow <a href="https://api.nasa.gov/index.html#apply-for-an-api-key" target="_blank">this link</a> and get it.</p>
         <p>Enter API key:</p>
         <input type="text" [(ngModel)]="apiKey">
         <button (click)="saveApiKey()">Submit</button>
